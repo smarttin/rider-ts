f9a117b2c768d9e896b9c285576d0f57c555c697
smarttin <smarttin77@gmail.com>
2020-10-18 21:21:03 +0100

    Add updateRideStatus mutation

---
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -0,0 +1,69 @@
+import Ride from '../../../entities/Ride';
+import User from '../../../entities/User';
+import {UpdateRideStatusMutationArgs, UpdateRideStatusResponse} from '../../../types/graph';
+import {Resolvers} from '../../../types/resolvers';
+import privateResolver from '../../../util/privateResolver';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateRideStatus: privateResolver(
+      async (
+        _,
+        args: UpdateRideStatusMutationArgs,
+        {req, pubSub},
+      ): Promise<UpdateRideStatusResponse> => {
+        const user: User = req.user;
+        if (user.isDriving) {
+          try {
+            let ride: Ride | undefined;
+            if (args.status === 'ACCEPTED') {
+              ride = await Ride.findOne(
+                {
+                  id: args.rideId,
+                  status: 'REQUESTING',
+                },
+                {relations: ['passenger']},
+              );
+              if (ride) {
+                ride.driver = user;
+                user.isTaken = true;
+                user.save();
+              }
+            } else {
+              ride = await Ride.findOne({
+                id: args.rideId,
+                driver: user,
+              });
+            }
+            if (ride) {
+              ride.status = args.status;
+              ride.save();
+              pubSub.publish('rideUpdate', {RideStatusSubscription: ride});
+              return {
+                ok: true,
+                error: null,
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Can't update ride",
+              };
+            }
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: 'You are not a driver',
+          };
+        }
+      },
+    ),
+  },
+};
+
+export default resolvers;
